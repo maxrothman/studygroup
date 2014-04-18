9caecf623f1c7962345bfddf15bf6f22b73b6d3e
Jason Michalski <armooo@armooo.net>
2014-04-17 21:24:34 -0700

    Add Flask-Migrate support
    Add Flask-Migrate to manage database schema changes

--- manage.py
+++ manage.py
@@ -1,14 +1,12 @@
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
-from studygroup.application import create_app, db, create_baseline_data
+from studygroup.application import create_app, db
 
+app = create_app()
 
-manager = Manager(create_app)
-
-
-@manager.command
-def create_tables():
-    db.create_all()
-    create_baseline_data()
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
